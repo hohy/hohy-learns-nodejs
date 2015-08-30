9e3e5252c114a46a3a76dd8412ddebe699e1db41
Jan Hýbl <jonnyxxx@gmail.com>
2015-08-30 23:05:04 +0200

    Implementation of passport authentification via facebook.
    Some pieces still missing... like, save user info to db, flash messages,
    etc...

--- config/routes.js
+++ config/routes.js
@@ -3,7 +3,17 @@
  */
 
 var index = require('../routes/index');
+var isAuthenticated = require('../middlewares/authentification');
 
-module.exports = function(app) {
-  app.get('/', index);
+module.exports = function(app, passport) {
+  app.get('/', isAuthenticated, index);
+  app.get('/login', index.login);
+
+  // passport routes for facebook login
+  app.get('/auth/facebook', passport.authenticate('facebook'));
+  app.get('/auth/facebook/callback', passport.authenticate('facebook',
+                                      { successRedirect: '/',
+                                        failureRedirect: '/login',
+                                        failureFlash: false })
+                                      );
 }
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,8 @@
 module.exports = function(req, res, next) {
   res.render('index', { title: 'Express' });
 };
+
+/* GET login page. */
+module.exports.login = function(req, res, next) {
+  res.render('fb-login', { title: 'Express' });
+};
