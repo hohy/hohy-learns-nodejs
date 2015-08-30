04729b9d56bb86d7328dbdf5814901c3a90085dc
Jan Hýbl <jonnyxxx@gmail.com>
2015-08-30 23:10:18 +0200

    Passport implementation.
    Something went wrong with previous commit... so there is this one.

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-/**
- * Main module of the application
- */
-
-// Dependencies
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var session = require('express-session');
-var configurations = require('./config/keys.json');
-var app = express();
 
-app.set('configurations', configurations); // let the config. be accessible everywhere
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,20 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'keyboard cat',
-                  resave: false,
-                  saveUninitialized: true}));
-app.use(passport.initialize());
-app.use(passport.session());
 
-// load application configuration
-var configuration = require('./config/general')(app);
-
-// set up passport authentification
-require('./config/passport')(app, passport);
-
-// load routes definition
-var routes = require('./config/routes')(app, passport);
+app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
